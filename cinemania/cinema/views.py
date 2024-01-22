579e7e2067b06ccdfe98258ce2c08ca5d6e16c4d
Cyberod <jonatkeez@gmail.com>
2024-01-22 06:24:41 -0800

    still demoing and refreshing my knowledge on django

--- cinemania/cinema/views.py
+++ cinemania/cinema/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+genres = [
+    {'id': 1, 'name':'Movie 1'},
+    {'id': 2, 'name':'Movie 2'},
+    {'id': 3, 'name':'Movie 3'},
+]
+
+
+def home(request):
+    context = {'genres': genres}
+    return render(request, 'cinema/home.html', context)
+
+
+def movies(request, pk):
+    
+    film = None
+
+    for i in genres:
+        if i['id'] == int(pk):
+            film = i
+
+    context = {'movies': film}        
+
+    return render(request, 'cinema/movies.html', context)                                                               
--- cinemania/cinemania/urls.py
+++ cinemania/cinemania/urls.py
@@ -1,22 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
 
-def home(request):
-    return HttpResponse("Hello, world. You're at the cinemania index.")
-
-
-def movies(request):
-    return HttpResponse("Hello, world. You're now viewing all the movies.")                                                               
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', home),
-    path('movies/', movies),
+    path('', include('cinema.urls')),
 ]
