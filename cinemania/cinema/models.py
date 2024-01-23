5a50be068a57956e04dbf88bbbacd19a5262f448
Cyberod <jonatkeez@gmail.com>
2024-01-23 01:16:43 -0800

    creating my models

--- cinemania/cinema/models.py
+++ cinemania/cinema/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movies(models.Model):
+    genre = models.CharField(max_length=1000)
+    description = models.TextField(null=True, blank=True)
+    #favourites
+    #updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
