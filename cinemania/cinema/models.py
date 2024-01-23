a6f2ab936cab309a2196590e8acbadfbf61fd35b
Cyberod <jonatkeez@gmail.com>
2024-01-23 03:14:33 -0800

    made first migration

--- cinemania/cinema/models.py
+++ cinemania/cinema/models.py
@@ -9,3 +9,6 @@ class Movies(models.Model):
     #favourites
     #updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
