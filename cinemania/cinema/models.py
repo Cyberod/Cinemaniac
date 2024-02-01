9cafb2690ae21d202a923f9bb38768f0afbf4cf4
Cyberod <jonatkeex@gmail.com>
2024-02-01 14:08:05 -0500

    fixed the problem with the Genre model

--- cinemania/cinema/models.py
+++ cinemania/cinema/models.py
@@ -9,6 +9,9 @@ from django.db.models.deletion import CASCADE
 class Genre(models.Model):
     name = models.CharField(max_length=200, null=True)
     
+    def __str__(self): 
+        return self.name
+    
 
 
 
