ca9896600c6d8f9934b3b986ad36f398f94c15fa
Cyberod <jonatkeez@gmail.com>
2024-02-02 06:38:49 -0800

    fixing issues on models relationship

--- cinemania/cinema/models.py
+++ cinemania/cinema/models.py
@@ -19,6 +19,7 @@ class Movie(models.Model):
     description = models.TextField(null=True)
     genre = models.ManyToManyField(Genre)
     release_date = models.DateField(null=True)
+    #favourited_movies = models.ManyToManyField('FavouriteMovie', through='Viewer', blank=True)
     #viewer = models.ManyToManyField(Viewer, blank=True)
     #favourite = models.ManyToManyField('Favourites', blank=True)
     #favourite = models.ForeignKey(Favourites, on_delete=CASCADE, null=True)
@@ -34,8 +35,10 @@ class Viewer(models.Model):
     L_name = models.CharField(max_length=200, null=True)
     phone = models.IntegerField(null=True)
     email = models.CharField(max_length=200, null=True)
-    favourites = models.ManyToManyField('Movie', through='FavouriteMovie')
+    favourites = models.ManyToManyField(Movie, through='FavouriteMovie')
     created = models.DateTimeField(auto_now_add=True, null=True)
+
+
 
     def __str__(self):
         return self.F_name.username
--- cinemania/cinema/views.py
+++ cinemania/cinema/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Genre, Movie
+from .models import Genre, Movie, Viewer, FavouriteMovie
 #from .forms import FavouriteForm
 # Create your views here.
 
@@ -15,9 +15,28 @@ from .models import Genre, Movie
 def home(request):
     genres = Genre.objects.all()
 
-    movies = Movie.objects.all()
+    # Assuming you have a Movie instance
+   # movie_instance = Movie.objects.get(id=1)
 
-    context = {'genres': genres, 'movies': movies}
+    # Accessing genres associated with the movie
+    #associated_genres = movie_instance.genres.all()
+
+    # Iterating through genres
+
+    viewer_instance = Viewer.objects.first()
+
+    viewer_name = viewer_instance.F_name.username
+
+
+    #favourite_movies = FavouriteMovie.objects.filter(viewer=viewer_instance)
+
+    favourite_movies = viewer_instance.favourites.prefetch_related('movie').all()
+
+
+    
+
+
+    context = {'genres': genres, 'favourite_movies': favourite_movies, 'viewer_name': viewer_name}
     return render(request, 'cinema/home.html', context)
 
 
